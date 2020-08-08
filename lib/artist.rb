660b6110b8eb38ec9fe7f6f46f67559a134e84f4
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-08 03:51:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,16 +12,12 @@ class Artist
 
   @@artists = []
   
-  def initialize
-    self.class.all << self
-    super
-    @songs = []
-end
+  
 
-  # def initialize
-  #   @@artists << self
-  #   @songs = []
-  # end
+  def initialize
+    @@artists << self
+    @songs = []
+  end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
